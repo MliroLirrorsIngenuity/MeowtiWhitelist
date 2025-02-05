37952116d4d726de42bfbb5a2521aed35419e246
Lemon-miaow <admin@lemonmiaow.xyz>
2025-02-05 20:22:50 +0800

    refactor: restructure command tree, add example for custom API

--- mcdr_ygg_whitelist_manager/__init__.py
+++ mcdr_ygg_whitelist_manager/__init__.py
@@ -6,18 +6,15 @@ from .utils import *
 
 server_dirname = 'server'
 world_dirname = 'world'
-api_methods = {
-    1: get_mojang_uuid_sync,
-    2: get_littleskin_uuid_sync
-}
 
 
 def on_load(server: PluginServerInterface, prev):
     server.register_help_message('!!whitelist', '白名单管理插件')
+    register_command(server)
 
 
-def server_cmd(info: Info, command: str):
-    info.get_server().execute(command)
+def server_cmd(src, command: str):
+    src.get_server().execute(command)
 
 
 def check_dir(dir_path):
@@ -51,41 +48,51 @@ def get_playerdata_filename_list():
     return playerdata_name_list
 
 
-def list_whitelist(info: Info):
-    whitelist_list: list = json_file_to_list(os.path.join(server_dirname, 'whitelist.json'))
-    log(info, '===== 白名单 =====：')
-    for i in range(0, len(whitelist_list)):
-        player_name, player_uuid = whitelist_list[i]['name'], whitelist_list[i]['uuid']
-        log(info, f'{i+1}: {player_name} - {player_uuid}')
-
-
-def show_help_msg(info: Info):
-    log(info, '''
+def show_help_msg(src):
+    log(src,'''
     =======================白名插件=======================
     !!whitelist add [name] (API num/name)    添加白名单
     !!whitelist remove [name]                移除白名单
     !!whitelist list                         显示完整白名单
     !!whitelist help                         显示此帮助信息
     ======================================================''')
-    show_choice_msg(info)
+    show_choice_msg(src)
 
 
-def show_choice_msg(info: Info):
-    log(info, f'''
+def show_choice_msg(src):
+    log(src,f'''
     API 可用选项：
     1 = Mojang
     2 = LittleSkin
     ''')
 
 
-def add_whitelist(info: Info, player_name: str, api: int):
+def add_whitelist(src, player_name: str, api: str):
+    api_methods = {
+        1: get_mojang_uuid_sync,
+        2: get_littleskin_uuid_sync
+#       3: example
+    }
+    if api.casefold() == 'mojang':
+        api = 1
+    elif api.casefold() == 'littleskin':
+        api = 2
+#   elif api.casefold() == 'example':
+#       api = 3
+    elif api.isdigit() and int(api) in api_methods:
+        api = int(api)
+    else:
+        log(src, "无效的 API 参数")
+        show_choice_msg(src)
+        return
+
     whitelist_list: list = json_file_to_list(os.path.join(server_dirname, 'whitelist.json'))
     whitelist_name_list = []
     for value in whitelist_list:
         whitelist_name_list.append(value['name'])
 
     if player_name in whitelist_name_list:
-        log(info, f'添加 {player_name} 白名时出现错误：白名单中已存在该玩家。')
+        log(src, f'添加 {player_name} 白名时出现错误：白名单中已存在该玩家。')
     else:
         uuid = api_methods.get(api, None)(player_name)
 
@@ -94,67 +101,66 @@ def add_whitelist(info: Info, player_name: str, api: int):
             whitelist_list.append(new_whitelist_dict)
             create_whitelist_file(whitelist_list, os.path.join(server_dirname), '_A_')
             time.sleep(1)
-            server_cmd(info, '/whitelist reload')
-            log(info, f'添加 {player_name} 白名单成功。')
+            server_cmd(src, '/whitelist reload')
+            log(src, f'添加 {player_name} 白名单成功。')
         else:
-            log(info, f'添加 {player_name} 白名单时出现错误，请重试')
+            log(src, f'添加 {player_name} 白名单时出现错误，请重试')
 
 
-def remove_whitelist(info: Info, player_name: str):
+def remove_whitelist(src, player_name: str):
     whitelist_list: list = json_file_to_list(os.path.join(server_dirname, 'whitelist.json'))
     for value in whitelist_list:
         if player_name == value['name']:
             sub_dict = {'uuid': value['uuid'], 'name': value['name']}
             whitelist_list.remove(sub_dict)
     create_whitelist_file(whitelist_list, os.path.join(server_dirname), '_R_')
-    log(info, f'白名单已移除玩家: name: {player_name}.')
+    log(src, f'白名单已移除玩家: {player_name}.')
 
 
-def on_user_info(server: PluginServerInterface, info: Info):
-    if info.content.startswith('!!whitelist'):
-        args = info.content.split(' ')
+def list_whitelist(src):
+    whitelist_list: list = json_file_to_list(os.path.join(server_dirname, 'whitelist.json'))
+    log(src, '===== 白名单 =====：')
+    for i in range(0, len(whitelist_list)):
+        player_name, player_uuid = whitelist_list[i]['name'], whitelist_list[i]['uuid']
+        log(src, f'{i+1}: {player_name} - {player_uuid}')
 
-        try:
-            if args[1] == 'add':
-                if server.get_permission_level(info) >= 3:
-                    if len(args) > 2:
-                        if len(args) > 3:
-                            if args[3].casefold() == 'mojang':
-                                api = 1
-                            elif args[3].casefold() == 'littleskin':
-                                api = 2
-                            elif args[3] in api_methods:
-                                api = int(args[3])
-                            else:
-                                log(info, '无效的 API 参数')
-                                show_choice_msg(info)
-                                return
-                        else:
-                            api = 1
-                        add_whitelist(info, args[2], api)
-                    else:
-                        log(info, '玩家名不能为空')
-                else:
-                    log(info, '权限不足')
 
-            elif args[1] == 'remove':
-                if server.get_permission_level(info) >= 3:
-                    if len(args) > 2:
-                        remove_whitelist(info, args[2])
-                    else:
-                        log(info, '玩家名不能为空')
-                else:
-                    log(info, '权限不足')
+def register_command(server: PluginServerInterface):
+    def get_literal_node(literal):
+        return Literal(literal)
 
-            elif args[1] == 'list':
-                if server.get_permission_level(info) >= 3:
-                    list_whitelist(info)
-                else:
-                    log(info, '权限不足')
-
-            elif args[1] == 'help':
-                show_help_msg(info)
-            else:
-                log(info, f'Error')
-        except IndexError:
-            show_help_msg(info)
+    server.register_command(
+        Literal("!!whitelist")
+        .requires(lambda src: src.has_permission(3))
+        .on_error(
+            RequirementNotMet,
+            lambda src: log(src, "权限不足"),
+            handled=True,
+        )
+        .runs(lambda src: show_help_msg(src))
+        .then(
+            get_literal_node("help")
+            .runs(lambda src: show_help_msg(src))
+        )
+        .then(
+            get_literal_node("add")
+            .runs(lambda src: log(src, "需要玩家名，正确用法：!!whitelist add [name] (API num/name)"))
+            .then(
+                Text("player_name").runs(lambda src: log(src, "需要API参数，正确用法：!!whitelist add [name] (API num/name)"))
+                .then(
+                    Text("api").runs(lambda src, ctx: add_whitelist(src, ctx["player_name"], str(ctx["api"])))
+                )
+            )
+        )
+        .then(
+            get_literal_node("remove")
+            .runs(lambda src: log(src, "需要玩家名，正确用法：!!whitelist remove [name]"))
+            .then(
+                Text("player_name").runs(lambda src, ctx: remove_whitelist(src, ctx["player_name"]))
+            )
+        )
+        .then(
+            get_literal_node("list")
+            .runs(lambda src: list_whitelist(src))
+        )
+    )
