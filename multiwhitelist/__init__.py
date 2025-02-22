f97077e91e69f124a33916fc1883d840b0232029
Lemon-miaow <admin@lemonmiaow.xyz>
2025-02-22 20:21:43 +0800

    refactor: move command registration to a separate module

--- multiwhitelist/__init__.py
+++ multiwhitelist/__init__.py
@@ -1,64 +1,12 @@
 from mcdreforged.api.all import *
 
 from multiwhitelist.constants import PREFIX
-from multiwhitelist.utils.config_utils import config
 from multiwhitelist.utils.file_utils import create_example_files
-from multiwhitelist.utils.logger_utils import log, log_available_apis
 from multiwhitelist.utils.translater_utils import tr
-from multiwhitelist.operations import add_whitelist, remove_whitelist, list_whitelist
+from multiwhitelist.command import register_command
 
 
 def on_load(server: PluginServerInterface, prev):
     server.register_help_message(PREFIX, tr("help_msg_name"))
     create_example_files()
     register_command(server)
-
-
-def register_command(server: PluginServerInterface):
-    def get_literal_node(literal):
-        return Literal(literal)
-
-    def show_help(src: CommandSource):
-        log(src, tr("help_msg", PREFIX))
-        log_available_apis(src)
-
-    server.register_command(
-        Literal(PREFIX)
-        .requires(lambda src: src.has_permission(config.permission))
-        .on_error(
-            RequirementNotMet,
-            lambda src: log(src, tr("error.permission_denied")),
-            handled=True,
-        )
-        .runs(lambda src: show_help(src))
-        .then(
-            get_literal_node("help")
-            .runs(lambda src: show_help(src))
-        )
-        .then(
-            get_literal_node("add")
-            .runs(lambda src: log(src, tr("error.add_require_name", PREFIX)))
-            .then(
-                Text("player_name").runs(lambda src: log(src, tr("error.require_api", PREFIX)))
-                .then(
-                    Text("api").runs(lambda src, ctx: add_whitelist(src, ctx["player_name"], ctx["api"]))
-                    .on_error(
-                        RequirementNotMet,
-                        lambda src, ctx: log(src, tr("error.unknown_error", ctx["player_name"])),
-                        handled=True,
-                    )
-                )
-            )
-        )
-        .then(
-            get_literal_node("remove")
-            .runs(lambda src: log(src, tr("error.remove_require_name", PREFIX)))
-            .then(
-                Text("player_name").runs(lambda src, ctx: remove_whitelist(src, ctx["player_name"]))
-            )
-        )
-        .then(
-            get_literal_node("list")
-            .runs(lambda src: list_whitelist(src))
-        )
-    )
