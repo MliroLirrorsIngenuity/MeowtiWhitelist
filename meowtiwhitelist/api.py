88ea56a59193e0a0154e4cb8ba891c5349d2137d
Lemon-miaow <admin@lemonmiaow.xyz>
2025-03-17 01:18:53 +0800

    refactor(api): simplify get_player_uuid and add whitelist management functions

--- meowtiwhitelist/api.py
+++ meowtiwhitelist/api.py
@@ -1,23 +1,18 @@
-from typing import Optional
-
-from meowtiwhitelist import tr
-from meowtiwhitelist.utils.logger_utils import log
+from meowtiwhitelist.operations import (
+    add_whitelist as _raw_add_whitelist,
+    remove_whitelist as _raw_remove_whitelist
+)
+from meowtiwhitelist.utils.uuid_utils.service_loader import build_service_mapping
 from meowtiwhitelist.utils.uuid_utils.uuid_utils import fetchers
 
 
-def get_player_uuid(src, service_id: int, player_name: str) -> Optional[str]:
-    if service_id not in fetchers:
-        log(src, tr("api.error.not_configured", service_id))
-        return None
+def get_player_uuid(player_name: str, service_id: str):
+    return fetchers[build_service_mapping()[service_id.strip().lower()]](player_name)
 
-    uuid_func = fetchers[service_id]
-    uuid = uuid_func(player_name)
 
-    if isinstance(uuid, int):
-        log(src, tr("api.error.status_code", uuid))
-        return None
-    elif uuid is None:
-        log(src, tr("api.error.unknown_error", player_name))
-        return None
+def add_whitelist(src, player_name: str, service_id: str):
+    _raw_add_whitelist(src, player_name, service_id)
 
-    return uuid
+
+def remove_whitelist(src, player_name: str):
+    _raw_remove_whitelist(src, player_name)
